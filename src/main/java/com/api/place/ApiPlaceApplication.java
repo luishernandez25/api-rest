2aa960e59394270d6a67b5edfe118db69123d243
luish25 <luishernandez130189@gmail.com>
2020-11-23 13:19:41 +0100

    gradle y primer test de la api hardcode

---
+++ src/main/java/com/api/place/ApiPlaceApplication.java
@@ -0,0 +1,13 @@
+package com.api.place;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPlaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPlaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/place/ApiPlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.place;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPlaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
