dcb53eb717fb2f37f6f37ac70c0cfe2430ea6ec3
luish25 <luishernandez130189@gmail.com>
2020-11-26 13:21:52 +0100

    se mejora la documentacion de swagger

--- src/main/java/com/api/rest/web/config/SwaggerConfig.java
+++ src/main/java/com/api/rest/web/config/SwaggerConfig.java
@@ -3,7 +3,9 @@ package com.api.rest.web.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.RequestHandler;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -17,6 +19,16 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.api.rest.web.controller"))
+                .build().apiInfo(apiEndInfo());
+
+    }
+
+    private ApiInfo apiEndInfo (){
+        return  new ApiInfoBuilder().title("Api Rest By Luis Hernandez")
+                .description("Desarrollo de API para aplicacion web de cursos online, Para su uso debes primero invocar el auth-controller el cual te devolvera un token para poder interactuar con las demas apis")
+                .license("Apache 2.0")
+                .version("1.0.0")
+                .licenseUrl("www.apache.org/licences/LICENCE-2.0.html")
                 .build();
 
     }
